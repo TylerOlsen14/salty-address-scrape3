678ee191f384f259583d17da5713ed01de4f9995
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-05-08 12:58:54 -0600

    managed to put scraped data into array that I can show in DOM

--- salty-scrape/src/App.js
+++ salty-scrape/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import array from './address.json'
+console.log(array)
 
 function App() {
   return (
     <div className="App">
-      
+      {array.map( scrape => (
+        <h1>{scrape.name}</h1>
+      ))}
     </div>
   );
 }
--- salty-scrape/src/components/address.js
+++ salty-scrape/src/components/address.js
@@ -1,15 +1,24 @@
 const fs = require('fs')
 
+let addressScrape = []
+
+const addressArray = () => {
 fs.readFile('../../../address.json', 'utf8', (err, jsonString) => {
-// fs.readFile('./tester.json', 'utf8', (err, jsonString) => {
+  // fs.readFile('./tester.json', 'utf8', (err, jsonString) => {
   if (err) {
     console.log("File read failed:", err)
-    return
   }
   try {
-    const address = JSON.parse(jsonString)
-    console.log('URL:', address)
-  } catch(err) {
+    const address = JSON.parse(jsonString) 
+    addressScrape = address
+    console.log(addressScrape)
+  }
+   catch(err) {
     console.log('Error parsing JSON String:', err)
   }
 })
+}
+const readArray = async () => {
+  await addressArray()
+  await console.log(addressScrape)
+}
