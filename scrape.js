cc8385a3da19f7307cfed01e53c9f363e16eabaa
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-05-08 09:43:04 -0600

    address grab works

--- scrape.js
+++ scrape.js
@@ -1,19 +1,18 @@
-const request = require('request')
-const cheerio = require('cheerio')
-const lineReader = require('line-reader')
+const request = require("request");
+const cheerio = require("cheerio");
+const lineReader = require("line-reader");
 
-lineReader.eachLine('./url.csv', function(line, last) {
-  const url = line
-  // console.log(url)
+lineReader.eachLine("./url.csv", function(line, last) {
+  const url = line;
 
-
-request(url, (error, response,html) => {
-  if(!error && response.statusCode == 200){
-    const $ = cheerio.load(html);
-    const td = $('table tr td table tr td table tr:nth-child(4) td:nth-child(1)');
-    const address = (td.text())
-    // const address = (td.text().slice(0,70))
-    console.log(address)
-  }
-})
-})
+  request(url, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+      const td = $(
+        "table tr td table tr td table tr:nth-child(4) td:nth-child(1)"
+      );
+      const address = td.text();
+      console.log(address);
+    }
+  });
+});
